080a72d9bbfd0888ac71a0971f9099e4efaf543c
Kelvin <kelvin.roche@upr.edu>
2019-03-12 20:45:53 -0500

    Added ExploredNode class.

---
+++ ExploredNode.java
@@ -0,0 +1,28 @@
+
+/**
+* A Node to save a state when explored, to ease the backtracking.
+* Author: Kelvin Roche
+* Last Update: 3/12/2019
+*/
+public class ExploredNode
+{
+  private PuzzleState state;
+  private PuzzleState previousState;
+
+  public ExploredNode(PuzzleState state, PuzzleState previousState)
+  {
+    this.state = state;
+    this.previousState = previousState;
+  }
+
+  public getState()
+  {
+    return state;
+  }
+
+  public getPrev()
+  {
+    return previousState;
+  }
+
+}
